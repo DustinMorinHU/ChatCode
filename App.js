c1dded28e49f70c879c3fba5f3948b221129d9f8
Tyler <tabrubaker@my.harrisburgu.edu>
2019-10-09 12:33:46 -0400

    Open new window when clicking the video calling button in the ChatCode.html

--- App.js
+++ App.js
@@ -1,2 +1,26 @@
-const react = require ('react')
+console.log('renderer process 2');
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path')
+const url = require('url')
+const remote = require('electron');
+const electron = require('electron');
+const app = electron.app
+
 console.log("REACT!")
+
+const Button = document.getElementById('VidCall');
+Button.addEventListener('click', function(event) {
+  let vid = new BrowserWindow({ width: 1280, height: 720 });
+  vid.loadURL(url.format({
+      pathname: path.join(__dirname, 'VidCall.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+    window.close()
+  });
+
+
+
+
+
+  // Or load a local HTML file
